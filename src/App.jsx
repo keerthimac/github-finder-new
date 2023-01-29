93e26427b8fcf7bb37fe497b9dd3e1ef24b0bc3f
Darshana <keerthi.mac@gmail.com>
2023-01-29 08:29:12 +0530

    Context Explained
    -We can manage states via Global states with this method
    -Moved main functions and states to this GithubContext file.
    
    -to make available this state to components,
        -warp parent elements with <GithubProvider> created with in GithubContext
        -in the component we can destructure this state using useContext hook like props destructure
        -and feed GithubContext to useContext() function
        -it is exactly like general operation. but state location changed.
    
    -this will be last step using useState. so watchout for code

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,26 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
+import { GithubProvider } from "./components/context/github/GithubContext";
+
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/*' element={<NotFound />} />
-            <Route path='/notfound' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/*' element={<NotFound />} />
+              <Route path='/notfound' element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
