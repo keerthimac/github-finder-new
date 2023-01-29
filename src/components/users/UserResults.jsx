3fe4b2ebee222b4bafdf831a0e6b9f22cdf2606f
Darshana <keerthi.mac@gmail.com>
2023-01-29 07:16:29 +0530

    UserItem components & UserResults Update
    -Updated layout with tailwind classes grid. Really important!
    -added spinner component to userResults
    
    -UserItem component created
    -important thing is really easy to implement a created
    -used flex for card inside

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./userItem";
 
 function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -15,14 +20,21 @@ function UserResults() {
       }
     );
     const data = await response.json();
-    console.log(data);
+    setUsers(data);
+    setLoading(false);
   };
 
-  return (
-    <div>
-      <h1>User Results</h1>
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
